956218323f7479e1500afaa42ad534f22f9aee86
gaurav20619 <gauravsinghraijada619@gmail.com>
2022-12-12 18:32:47 +0530

    Add: Welcome message for stack and queue.

---
+++ src/main/java/com/bridgelabz/stackandqueue/StackandqueueApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.stackandqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackandqueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StackandqueueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bridgelabz/stackandqueue/StackandqueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.stackandqueue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StackandqueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
